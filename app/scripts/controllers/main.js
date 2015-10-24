73a4d19f541617d5b6f2f37fbfdb6a94ce3a224b
Sarath <sarath.hacks@gmail.com>
2015-10-24 20:06:07 +0530

    Started working for V2

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bookSearchApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bookSearchApp
+ */
+angular.module('bookSearchApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
